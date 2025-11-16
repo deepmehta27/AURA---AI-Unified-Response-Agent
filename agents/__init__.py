c4ea5a3ac412ef8d1d178b370292dbbabaced340
deep <deepmehta827@gmail.com>
2025-11-16 14:04:05 -0500

    added image_agent and orchestrator

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,7 @@
 
 from .base_agent import BaseAgent
 from .text_agent import text_agent
+from .image_agent import image_agent
+from .orchestrator import orchestrator
 
-__all__ = ['BaseAgent', 'text_agent']
+__all__ = ['BaseAgent', 'text_agent', 'image_agent', 'orchestrator']
