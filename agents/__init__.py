2591dfd21fc232e887283acda4a575e8b6bc7fbd
deep <deepmehta827@gmail.com>
2025-11-19 11:37:39 -0500

    added audio_agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,6 +3,7 @@
 from .base_agent import BaseAgent
 from .text_agent import text_agent
 from .image_agent import image_agent
+from .audio_agent import audio_agent
 from .orchestrator import orchestrator
 
-__all__ = ['BaseAgent', 'text_agent', 'image_agent', 'orchestrator']
+__all__ = ['BaseAgent', 'text_agent', 'image_agent', 'audio_agent', 'orchestrator']
